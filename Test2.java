54fbf2cea94b877a06bea724ebf6accb047b91a8
Dale R. Anderson <lime.dale@gmail.com>
2009-02-14 16:38:18 -0800

    Renders a text table to stdout.

--- Test2.java
+++ Test2.java
@@ -10,13 +10,13 @@ public class Test2 {
 			//
 			new Ball("Orange ball", "orange", 
 					2.0, 5, 
-					150, 250, 6, 2),
+					150, 250, 16, 12),
 			new Ball("White ball", "white",   
 					2.0, 5, 
-					630, 200, 4, 5),
+					630, 200, 40, 5),
 			new Ball("Green ball", "green",   
 					2.1, 5.01, 
-					250, 285, -6, -2)
+					250, 285, -6, -20)
 
 			});
 
@@ -47,11 +47,10 @@ public class Test2 {
 
 			theRack.collisions();
 
-			//Clear the screen
-			System.out.format("%c[2J", 0x1B);
-
-			//Print the report
-			System.out.println(theRack.report());
+			render(Table, theRack);
+			
+			System.err.format("%c[2J", 0x1B);
+			System.err.println(theRack.report());
 
 			try {
 
@@ -67,7 +66,67 @@ public class Test2 {
 
 	}
 
+	public static void render(Felt felt, Rack rack)
+	{
+			//Draw a table 80x20
+			StringBuffer table = new StringBuffer(
+"+------------------------------------------------------------------------------+\n"
+			);
 
+			for(int v=1; v<=18; v++)
+			{
+				table.append("|");
+
+				double yMin, yMax;
+
+				yMin = (felt.height() / 18) * (v-1);
+				yMax = (felt.height() / 18) * v;
+
+				for(int h=1; h<=78; h++)
+				{
+					double xMin, xMax;
+
+					xMin = (felt.width() / 78) * (h-1);
+					xMax = (felt.width() / 78) * h;
+
+					boolean foundOne=false;
+
+					for(int b=0; b < rack.set.length; b++)
+					{
+						double x=rack.set[b].location.x();
+						double y=rack.set[b].location.y();
+
+						if( (x > xMin && x <= xMax) &&
+						    (y > yMin && y <= yMax) )
+						{
+							foundOne=true;
+							break;
+						}
+
+					}
+
+					if(foundOne)
+						table.append("O");
+					else
+						table.append(" ");
+				}
+				table.append("|\n");
+			}
+
+			table.append(
+"+------------------------------------------------------------------------------+\n"
+			);
+
+
+			//Clear the screen
+			System.out.format("%c[2J", 0x1B);
+
+			////Print the report
+
+			System.out.println(table);
+
+
+	}
 
 
 }
