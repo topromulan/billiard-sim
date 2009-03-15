5e13af1278130c2546671ff263fa06793c2d119f
Dale R. Anderson <lime.dale@gmail.com>
2009-03-15 16:24:58 -0700

    Structured the test for multiple individual collision tests

--- Test2.java
+++ Test2.java
@@ -18,86 +18,21 @@ public class Test2 {
 		});
 
 
-
-
-
-//			new Ball("White ball", "white",
-//					2.0, 5, 
-//					50, 50, 30, 0),
-////			new Ball("Red ball", "red", 
-//					2.0, 5, 
-//					450, 50, -10, -20),
-//			new Ball("Green ball", "green", 
-//					2.0, 5, 
-//					470, 50, -20, -15),
-//			new Ball("Blue ball", "blue", 
-////					2.0, 5, 
-//					400, 150, 25, -20),
-//
-//
-
-
-//			new Ball("Red ball", "red", 
-//					2.0, 5, 
-//					10, 10, 10, 0),
-//			new Ball("Target 1", "t1", 
-//					2.0, 5, 
-//					260, 10, 0, 0),
-//					
-//			new Ball("Orange ball", "orange", 
-//					2.0, 5, 
-//					10, 45, 10, 0),
-//			new Ball("Target 2", "t2", 
-//					2.0, 5, 
-//					260, 50, 0, 0),
-//					
-//			new Ball("Yellow ball", "yellow", 
-//					2.0, 5, 
-//					10, 90, 10, 0),
-//			new Ball("Target 3", "t3", 
-//					2.0, 5, 
-//					260, 90, 0, 0),
-//
-//			new Ball("Green ball", "green", 
-//					2.0, 5, 
-//					10, 130, 10, 0),
-//			new Ball("Target 4", "t4", 
-//					2.0, 5, 
-//					260, 130, 0, 0),
-//
-//			new Ball("Blue ball", "blue", 
-//					2.0, 5, 
-//					10, 170, 10, 0),
-//			new Ball("Target 5", "t5", 
-//					2.0, 5, 
-//					260, 170, 0, 0),
-//
-//			new Ball("Indigo ball", "indigo",   
-//					2.0, 5, 
-//					10, 210, 10, 0),
-//			new Ball("Target 6", "t6", 
-//					2.0, 5, 
-//					260, 210, 0, 0),
-//
-//			new Ball("Violet ball", "violet",   
-//					2.0, 5, 
-//					410, 50, 10, 0),
-//			new Ball("Target 7", "t7", 
-//					2.0, 5, 
-//					408, 150, 10, -10)
-//
-//			});
-
 		Felt Table = new Felt(640, 320); // pool table is domino shaped
 						// adjust for screen ratio in
 						// higher layer
+						//
+
+		runTable(Table, theRack);
+
+
+	}
 
 
 		
+	static void runTable(Felt table, Rack rack) {
 
-		System.out.println(theRack.report());
-
-
+		System.out.println(rack.report());
 		
 			try {
 
@@ -105,18 +40,17 @@ public class Test2 {
 			} catch (Exception e) {}
 		
 
-
 		System.err.format("%c[2J", 0x1B);
 		System.err.println("New pool hall test.\n----------");
 		
 
 		do {
 
-			Table.rub(theRack); 
+			table.rub(rack); 
 
-			theRack.collisions();
+			rack.collisions();
 
-			render(Table, theRack);
+			render(table, rack);
 			
 			//System.err.format("%c[2J", 0x1B);
 			//System.err.println(theRack.report());
@@ -126,7 +60,7 @@ public class Test2 {
 				java.lang.Thread.sleep(100);
 			} catch (Exception e) {}
 
-		} while ( theRack.hasMotion( ) );
+		} while ( rack.hasMotion( ) );
 
 
 		System.out.println("Test complete.");
@@ -135,6 +69,8 @@ public class Test2 {
 
 	}
 
+	//Well I should classify this somewhere else since it is a fine
+	// upstanding text mode billiards table.
 	public static void render(Felt felt, Rack rack)
 	{
 			//Draw a table 80x20
