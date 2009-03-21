3a4281efda55369f3212072608796f4fcd3222bc
Dale R. Anderson <lime.dale@gmail.com>
2009-03-21 14:54:39 -0700

    test3 saves/modifies TTY settings

--- test3.sh
+++ test3.sh
@@ -1,1 +1,10 @@
-javac SimpleVector.java SimpleCoordinates.java  Ball.java Test3.java && java Test3
+javac SimpleVector.java SimpleCoordinates.java  Ball.java Test3.java ||
+	exit 1
+
+OLDSTTY=`stty -g`
+
+stty -echo -icanon min 1
+
+java Test3
+
+stty $OLDSTTY
